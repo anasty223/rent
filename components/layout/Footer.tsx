55aa13a846cab528742bfc341918075a9d958a05
anasty223 <anasty22@gmsil.com>
2026-03-09 19:09:06 +0200

    Refactor Footer component to use CSS background image instead of Image component

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,28 +1,17 @@
-import Image from "next/image";
-
 const Footer = () => {
   return (
-    <footer className="relative w-full flex flex-col items-center dark:bg-black overflow-hidden">
-      <div className="relative w-full h-[557px]">
-       
-        <Image
-          src="/images/Copyright.png"
-          alt="Footer background"
-          fill
-          className="object-cover object-center " // opacity по желанию
-          priority
-        />
-
-
-        <div className="relative z-10 w-full max-w-[1440px] mx-auto  flex items-end px-[80px] pb-[40px] h-[70px] bg-[rgba(17,17,17,0.8)] ">
-          <p className="text-white text-[14px] font-normal">
+    <footer
+      className="relative w-full flex flex-col items-center dark:bg-black overflow-hidden bg-cover bg-center"
+      style={{ backgroundImage: 'url(/images/Copyright.png)' }}
+    >
+      <div className="w-full h-[557px] flex items-end justify-center">
+        <div className="w-full   py-[20px] h-[70px] bg-[rgba(17,17,17,0.8)]">
+        <div className="max-w-[1440px] mx-auto flex items-center justify-between px-[80px]">  <p className="text-white text-[14px] font-normal">
             © Copyright 2021, «Домашняя гостиница»
           </p>
-          <div className="flex-1" />
-          <p className="text-white text-[14px] font-normal text-center flex-1">
+          <p className="text-white text-[14px] font-normal text-center">
             Пользовательское соглашение
-          </p>
-          <div className="flex-1" />
+          </p></div>
         </div>
       </div>
     </footer>
